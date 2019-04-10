67c25e420e2f7829690dd0efdd9371e870f497fc
Ben Patterson <ben@robotjamie.com>
2019-04-10 12:26:16 +1000

    Add all libaries for trellis

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ ARDUINO_DIR:=$(shell ./bin/arduino-cli --config-file ../arduino-cli.yaml config 
 ARDUINO_PACKAGE_DIR=$(ARDUINO_DIR)/packages
 ARDUINO_SKETCHBOOK=$(ROOT)
 
+ARDUINO_VERSION=10803
+
 ARDMK_VENDOR=adafruit
 BOARD_TAG=adafruit_trellis_m4
 ARCHITECTURE=samd
@@ -14,12 +16,17 @@ BUNDLED_BOSSA_DIR=$(ARDUINO_PACKAGE_DIR)/$(ARDMK_VENDOR)/tools/bossac
 BOSSA_VER=$(shell echo "$(wildcard $(BUNDLED_BOSSA_DIR)/*)" | xargs -n 1 basename | sort -V | tail -1)
 BOSSA = $(BUNDLED_BOSSA_DIR)/$(BOSSA_VER)/bossac
 
+U8G_PATH:=$(ROOT)/libraries/u8g2/csrc/
+U8G_CPP_PATH:=$(ROOT)/libraries/u8g2/cppsrc/
+
+CPPFLAGS += -I. -I$(U8G_PATH) -I$(U8G_CPP_PATH)
+
 # This variable is local, name it however you want
-SOURCE_FOLDERS=src
+SOURCE_FOLDERS=src $(U8G_PATH) $(U8G_CPP_PATH)
 
 # These two are used by Arduino.mk
-# LOCAL_C_SRCS=$(foreach dir,$(SOURCE_FOLDERS),$(wildcard $(dir)/*.c))
-# LOCAL_CPP_SRCS=$(foreach dir,$(SOURCE_FOLDERS),$(wildcard $(dir)/*.cpp))
+LOCAL_C_SRCS=$(foreach dir,$(SOURCE_FOLDERS),$(wildcard $(dir)/*.c))
+LOCAL_CPP_SRCS=$(foreach dir,$(SOURCE_FOLDERS),$(wildcard $(dir)/*.cpp))
 LOCAL_INO_SRCS=$(foreach dir,$(SOURCE_FOLDERS),$(wildcard $(dir)/*.ino))
 
 BOSSA_OPTS = -d --info --erase --write --verify --reset --offset=0x4000
--- src/Lattice.ino
+++ src/Lattice.ino
@@ -1,94 +1,156 @@
+// Trellis M4 MIDI Keypad CC
+// sends 32 notes, pitch bend & a CC from accelerometer tilt over USB MIDI
+
+#include <Wire.h>
+#include <Adafruit_Keypad.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel_ZeroDMA.h>
+#include <Adafruit_ZeroDMA.h>
+#include <SPI.h>
+#include <MIDIUSB.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL343.h>
+#include <Adafruit_NeoTrellisM4.h>
+#include "/Users/ben/Documents/Arduino/libraries/U8g2/src/clib/u8g2.h"
+#include "/Users/ben/Documents/Arduino/libraries/U8g2/src/U8g2lib.h"
 
-#define PIN 10
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
 
-void colorWipe(uint32_t c, uint8_t wait);
-void rainbow(uint8_t wait);
-void rainbowCycle(uint8_t wait);
-void theaterChase(uint32_t c, uint8_t wait);
-void theaterChaseRainbow(uint8_t wait);
+
+#define MIDI_CHANNEL     0  // default channel # is 0
+// Set the value of first note, C is a good choice. Lowest C is 0.
+// 36 is a good default. 48 is a high range. Set to 24 for a bass machine.
+#define FIRST_MIDI_NOTE 36
+
+Adafruit_ADXL343 accel = Adafruit_ADXL343(123, &Wire1);
+
+int xCC = 1;  //choose a CC number to control with x axis tilting of the board. 1 is mod wheel, for example.
+
+int last_xbend = 0;
+int last_ybend = 0;
+
+Adafruit_NeoTrellisM4 trellis = Adafruit_NeoTrellisM4();
+
+float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax, bool clamp);
+
 uint32_t Wheel(byte WheelPos);
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(32, PIN, NEO_GRB + NEO_KHZ800);
+void setup(){
+  Serial.begin(115200);
+  //while (!Serial);
+  Serial.println("MIDI keypad & pitchbend!");
+    
+  trellis.begin();
+  trellis.setBrightness(80);
 
-void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  strip.setBrightness(100);
+  // USB MIDI messages sent over the micro B USB port
+  Serial.println("Enabling MIDI on USB");
+  trellis.enableUSBMIDI(true);
+  trellis.setUSBMIDIchannel(MIDI_CHANNEL);
+  // UART MIDI messages sent over the 4-pin STEMMA connector (3.3V logic)
+  // Serial.println("Enabling MIDI on UART");
+  // trellis.enableUARTMIDI(true);
+  // trellis.setUARTMIDIchannel(MIDI_CHANNEL);
+  
+  u8g2.begin();
+
+  if(!accel.begin()) {
+    Serial.println("No accelerometer found");
+    while(1);
+  }
 }
 
 void loop() {
-  rainbow(20);
-}
+  // put your main code here, to run repeatedly:
+  trellis.tick();
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
+  while (trellis.available()){
+    keypadEvent e = trellis.read();
+    int key = e.bit.KEY;
+    Serial.print("Keypad key: ");    Serial.println(key);
+    Serial.print("MIDI note: ");     Serial.println(FIRST_MIDI_NOTE+key);
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+    if (e.bit.EVENT == KEY_JUST_PRESSED) {
+      Serial.println(" pressed\n");
+      trellis.setPixelColor(key, 0xFFFFFF);
+      trellis.noteOn(FIRST_MIDI_NOTE+key, 64);
     }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
+    else if (e.bit.EVENT == KEY_JUST_RELEASED) {
+      Serial.println(" released\n");
+      trellis.setPixelColor(key, 0x0);
+      trellis.noteOff(FIRST_MIDI_NOTE+key, 64);
     }
   }
-}
 
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
+  // Check for accelerometer
+  sensors_event_t event;
+  accel.getEvent(&event);
+  /* Display the results (acceleration is measured in m/s^2) */
+  //Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  //Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  //Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+  int xbend = 0;
+  int ybend = 0;
 
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
+  if (abs(event.acceleration.y) < 2.0) {  // 2.0 m/s^2
+    // don't make any bend unless they've really started moving it
+    ybend = 8192; // 8192 means no bend
+  } else {
+    if (event.acceleration.y > 0) {
+      ybend = ofMap(event.acceleration.y, 2.0, 10.0, 8192, 0, true);  // 2 ~ 10 m/s^2 is downward bend
+    } else {
+      ybend = ofMap(event.acceleration.y, -2.0, -10.0, 8192, 16383, true);  // -2 ~ -10 m/s^2 is upward bend
     }
   }
+  if (ybend != last_ybend) {
+    Serial.print("Y pitchbend: "); Serial.println(ybend);
+    trellis.pitchBend(ybend);
+    last_ybend = ybend;
+  }
+
+  if (abs(event.acceleration.x) < 2.0) {  // 2.0 m/s^2
+    // don't make any bend unless they've really started moving it
+    xbend = 0;
+  } else {
+    if (event.acceleration.x > 0) {
+      xbend = ofMap(event.acceleration.x, 2.0, 10.0, 0, 127, true);  // 2 ~ 10 m/s^2 is upward bend
+    } else {
+      xbend = ofMap(event.acceleration.x, -2.0, -10.0, 0, 127, true);  // -2 ~ -10 m/s^2 is downward bend
+    }
+  }
+  if (xbend != last_xbend) {
+    Serial.print("X mod: "); Serial.println(xbend);
+    trellis.controlChange(xCC, xbend);  //xCC is set at top of sketch. e.g., CC 1 is Mod Wheel
+    last_xbend = xbend;
+  }
+
+  trellis.sendMIDI(); // send any pending MIDI messages
+
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+  u8g2.drawStr(0,10,"Hello World!");	// write something to the internal memory
+  u8g2.sendBuffer();					// transfer internal memory to the display
+
+  delay(10);
+}
+
+
+
+// floating point map
+float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax, bool clamp) {
+    float outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+
+    if (clamp) {
+      if (outputMax < outputMin) {
+        if (outVal < outputMax)  outVal = outputMax;
+        else if (outVal > outputMin)  outVal = outputMin;
+      } else {
+        if (outVal > outputMax) outVal = outputMax;
+        else if (outVal < outputMin)  outVal = outputMin;
+      }
+    }
+    return outVal;
+
 }
 
 // Input a value 0 to 255 to get a color value.
@@ -96,12 +158,12 @@ void theaterChaseRainbow(uint8_t wait) {
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return trellis.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return trellis.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
   WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  return trellis.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
